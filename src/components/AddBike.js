4f8bd17ee86d4c42d987cd983a68b2594530f0a8
mostlyclicks <carlos@mostlyclicks.com>
2021-08-04 19:18:47 -0600

    initial commit - addBike, addList

---
+++ src/components/AddBike.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddBike = () => {
+  return (
+    <div>
+      <h1>Add a Bike</h1>
+    </div>
+  )
+}
+
+export default AddBike
---
+++ src/components/BikeList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BikeList = () => {
+  return (
+    <div>
+      <h1>Bike List</h1>
+    </div>
+  )
+}
+
+export default BikeList
